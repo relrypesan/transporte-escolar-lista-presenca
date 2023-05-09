9f8c243063412cf2ee1568c187e72efd121835f6
relrypesan <relrypesan@gmail.com>
2023-05-09 00:40:27 -0300

    commit inicial

---
+++ src/main/java/me/relrypesan/transporteescolarlistapresenca/TransporteEscolarListaPresencaApplication.java
@@ -0,0 +1,13 @@
+package me.relrypesan.transporteescolarlistapresenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteEscolarListaPresencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporteEscolarListaPresencaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/relrypesan/transporteescolarlistapresenca/TransporteEscolarListaPresencaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.relrypesan.transporteescolarlistapresenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteEscolarListaPresencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
