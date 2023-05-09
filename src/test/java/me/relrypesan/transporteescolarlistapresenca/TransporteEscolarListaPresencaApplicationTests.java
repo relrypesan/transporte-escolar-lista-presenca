14363704c580d91e4f920dd07969d9cd2fdf1bd4
relrypesan <relrypesan@gmail.com>
2023-05-09 00:58:55 -0300

    removendo anotação de teste unitario ainda não implementado

--- src/test/java/me/relrypesan/transporteescolarlistapresenca/TransporteEscolarListaPresencaApplicationTests.java
+++ src/test/java/me/relrypesan/transporteescolarlistapresenca/TransporteEscolarListaPresencaApplicationTests.java
@@ -1,9 +1,7 @@
 package me.relrypesan.transporteescolarlistapresenca;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class TransporteEscolarListaPresencaApplicationTests {
 
 	@Test
